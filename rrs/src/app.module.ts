97c3e1a6c87062e7b7adee116fa7a28c45326178
Mostafa Shaalan <mostafashaalan80@gmail.com>
2023-09-24 03:09:00 +0300

    connected to postgres

--- rrs/src/app.module.ts
+++ rrs/src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { IntervalsModule } from './intervals/intervals.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, BooksModule, IntervalsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '718653',
+      database: 'reading_recomendation_system',
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+    BooksModule,
+    IntervalsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
